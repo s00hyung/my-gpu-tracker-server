d8e2527461c85ec7f8e8e9fe17902bf457a40fb7
SooHyung Lee <18585776+soohl@users.noreply.github.com>
2021-05-24 21:45:17 +0900

    setup cors

--- app/main.py
+++ app/main.py
@@ -1,14 +1,28 @@
 from fastapi import FastAPI, BackgroundTasks
+from fastapi.middleware.cors import CORSMiddleware
 from .crawler import *
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+app = FastAPI()
+
+
 def update_prices():
     start_crawl()
+
 
 @app.get("/")
 def read_root():
     return read_json()
+
 
 @app.get("/start")
 def start_crawling(background_tasks: BackgroundTasks):
